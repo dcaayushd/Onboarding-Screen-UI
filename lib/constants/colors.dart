124dd7da6f8824c837f415a72efa862a80a25577
Aayush <aayushdc123@yahoo.com>
2024-05-14 00:06:57 +0545

    Colors Added

---
+++ lib/constants/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color.fromRGBO(269, 89, 114, 80); // Reduce red by 20
+const Color secondaryColor =
+    Color.fromARGB(155, 159, 53, 36); // Reduce red by 20, increase green by 20
+
+const Color hintTextColor = Colors.grey;
+const Color whiteColor = Colors.white;
+
+final Color lightGrey = Colors.grey.shade400;
+final Color lightBlueGrey = Colors.blueGrey.shade300;
